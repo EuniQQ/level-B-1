18be0982550323d912a13b80e074c659d351ed4d
unknown <eunice911qq@gmail.com>
2021-12-26 21:48:15 +0800

    處理back/title頁的新增、更新、刪除功能，並以title檔複製為ad檔後微

--- 102201/back/title.php
+++ 102201/back/title.php
@@ -2,7 +2,7 @@
         <!-- <p class="t cent botli">網站標題管理</p> -->
         <!-- <p class="t cent botli"><?=$titleStr['title'];?></p> -->
         <p class="t cent botli"><?=$DB->title;?></p>
-        <form method="post" target="back" action="?do=tii">
+        <form method="post" action="api/edit_title.php">
     <table width="100%">
     	<tbody>
         <!-- 標題列     -->
@@ -14,13 +14,52 @@
                 <td></td>
             </tr>
 
+
+        <?php
+           //老師習慣以$rows代表多筆、複數的
+            $rows=$DB->all();
+            foreach($rows as $row){  //很多筆的其中一筆
+        ?>
+
+            <!-- 預設情況下，表單input的name欄位中的會被放到$_POST的KEY值去。 -->
+            <!-- 若是KEY值一樣就會被蓋過。所以要完整將資料送到後台，須將所有資料放到一個陣列中 -->
+            <!-- 只要一次送出多筆，name值一律加中誇括號 -->
+
+            <tr>
+        	    <td width="45%">
+                    <img src="./img/<?=$row['img'];?>" style="width:300px;height:30px">
+                </td>
+                <td width="23%">
+                    <input type="text" name="text[]" value="<?=$row['text'];?>">
+                </td>
+                <td width="7%">
+                    <input type="radio" name="sh" value="<?=$row['id'];?>" <?=$checked?>>
+                <!-- $checked是預設一筆選取，如果兩筆都是1(顯示)會以最下面一筆為準 -->
+                </td>
+                <td width="7%">
+                    <input type="checkbox" name="del[]" value="<?=$row['id'];?>">
+                    <!-- name是陣列型態才能一次刪多筆 -->
+                </td>
+                <td>
+            <!-- 只要牽扯到更新，就要知道對象是誰(對應的id) -->
+            <!-- 在每筆資料增加一個隱藏欄位，為了得到id -->
+            <input type="hidden" name="id[]" value="<?=$row['id'];?>">
+            <input type="button"                                                  
+                onclick="op(&#39;#cover&#39;,&#39;#cvr&#39;,&#39;modal/title.php&#39;)"
+                value="<?=$DB->button?>">  
+
+                </td>
+            </tr>
+
+        <?php  } ?>
+
     </tbody></table>
            <table style="margin-top:40px; width:70%;">
      <tbody><tr>
-      <td width="200px"><input type="button" 
-                                                           
-          onclick="op(&#39;#cover&#39;,&#39;#cvr&#39;,&#39;modal/title.php&#39;)" 
-          value="<?=$DB->button?>"></td>
+      <td width="200px"><input type="button"                                                  
+          onclick="op(&#39;#cover&#39;,&#39;#cvr&#39;,&#39;modal/update_title.php&#39;)" 
+          value="更新圖片">
+        </td>
           <td class="cent">
               <input type="submit" value="修改確定">
               <input type="reset" value="重置"></td>
